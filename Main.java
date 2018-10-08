c610963582ccbafc652307a6ec6f0961c6eb120d
wesleyhuhuhu <31289917+wesleyhuhuhu@users.noreply.github.com>
2018-10-08 12:28:46 -0700

    initialize array

--- Main.java
+++ Main.java
@@ -31,7 +31,7 @@ class Main
          */
         System.out.println("*** PRINTING ar1 *");
         
-                
+                int[] ar1 = new int[n];
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
